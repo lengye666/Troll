b54860036486d78a94ce2e137d8add276447e2b6
lengye666 <1750297226@qq.com>
2025-01-02 15:18:06 +0800

    Create LSApplicationWorkspace.h

---
+++ TrollStore/LSApplicationWorkspace.h
@@ -0,0 +1,14 @@
+#import <Foundation/Foundation.h>
+
+@interface LSApplicationWorkspace : NSObject
++ (instancetype)defaultWorkspace;
+- (NSArray *)installedApplications;
+@end
+
+@interface LSApplicationProxy : NSObject
+@property (nonatomic, readonly) NSString *bundleType;
+@property (nonatomic, readonly) BOOL installed;
+@property (nonatomic, readonly) NSString *bundleIdentifier;
+@property (nonatomic, readonly) NSString *bundlePath;
+@property (nonatomic, readonly) NSString *localizedName;
+@end
